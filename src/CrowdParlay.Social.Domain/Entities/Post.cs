521e4fca276816e827c890b7c3fa7c78b179e4b7
zendet <hozztie@gmail.com>
2023-04-07 04:06:07 +0700

    docs(post): #9, add a summary

--- src/CrowdParlay.Social.Domain/Entities/Post.cs
+++ src/CrowdParlay.Social.Domain/Entities/Post.cs
@@ -1,5 +1,8 @@
 namespace CrowdParlay.Social.Domain.Entities;
 
+/// <summary>
+/// Post with text, timestamp, OP (author) and discussion id. Any authenticated user can create post in any discussion.
+/// </summary>
 public class Post
 {
     public int Id { get; set; }
