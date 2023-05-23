9d33d73c498166359f6b7de0e5fffbce51337f4b
zendet <hozztie@gmail.com>
2023-05-23 23:37:55 +0700

    refactor: removed unnecessary 'default' keyword

--- src/CrowdParlay.Social.Application/DTOs/Author/AuthorDto.cs
+++ src/CrowdParlay.Social.Application/DTOs/Author/AuthorDto.cs
@@ -6,6 +6,6 @@ public class AuthorDto
 {
     public Guid Id { get; set; }
     public string Alias { get; set; } = default!;
-    public required string DisplayName { get; set; } = default!;
-    public required string AvatarUrl { get; set; } = default!;
+    public required string DisplayName { get; set; }
+    public required string AvatarUrl { get; set; }
 }
--- src/CrowdParlay.Social.Application/DTOs/Post/PostDto.cs
+++ src/CrowdParlay.Social.Application/DTOs/Post/PostDto.cs
@@ -6,7 +6,7 @@ namespace CrowdParlay.Social.Application.DTOs.Post;
 public class PostDto
 {
     public Guid Id { get; set; }
-    public required string Content { get; set; } = default!;
+    public required string Content { get; set; }
     public DateTime CreatedAt { get; set; }
-    public AuthorDto AuthorDto { get; set; } = default!;
+    public required AuthorDto AuthorDto { get; set; }
 }
