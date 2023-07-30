498ae9618712060db3d2fd68db4c4021bffbd8f5
Степной ишак <undrcrxwn@gmail.com>
2023-07-30 14:43:56 +0000

    refactor(di): maintain code-style

--- src/CrowdParlay.Social.Api/ApiServiceExtensions.cs
+++ src/CrowdParlay.Social.Api/ApiServiceExtensions.cs
@@ -4,9 +4,8 @@ namespace CrowdParlay.Social.Api;
 
 public static class ApiServiceExtensions
 {
-    public static IServiceCollection AddApi(this IServiceCollection services) =>
-           services
-               .AddSwaggerGen()
-               .AddEndpointsApiExplorer()
-               .AddTransient<ExceptionHandlingMiddleware>();
+    public static IServiceCollection AddApi(this IServiceCollection services) => services
+        .AddSwaggerGen()
+        .AddEndpointsApiExplorer()
+        .AddTransient<ExceptionHandlingMiddleware>();
 }
