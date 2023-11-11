d72a12f983e9767d039641a129eb6378db967a06
undrcrxwn <undrcrxwn@gmail.com>
2023-11-11 10:45:05 +0300

    tests: add JWT authorization and discussions support

---
+++ tests/CrowdParlay.Social.IntegrationTests/Services/Authorization.cs
@@ -0,0 +1,13 @@
+using JWT.Algorithms;
+using JWT.Builder;
+
+namespace CrowdParlay.Social.IntegrationTests.Services;
+
+public static class Authorization
+{
+    public static string ProduceJwt(string userId) => new JwtBuilder()
+        .WithAlgorithm(new HMACSHA256Algorithm())
+        .WithSecret(string.Empty)
+        .AddClaim("sub", userId)
+        .Encode();
+}
--- tests/CrowdParlay.Social.IntegrationTests/Tests/AuthorsControllerTests.cs
+++ tests/CrowdParlay.Social.IntegrationTests/Tests/AuthorsControllerTests.cs
@@ -1,12 +1,14 @@
 using System.Net;
 using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
 using CrowdParlay.Communication;
 using CrowdParlay.Social.Api.v1.DTOs;
-using CrowdParlay.Social.Application.DTOs.Author;
-using CrowdParlay.Social.Application.DTOs.Comment;
+using CrowdParlay.Social.Application.DTOs;
 using CrowdParlay.Social.IntegrationTests.Fixtures;
 using FluentAssertions;
 using MassTransit.Testing;
+using Authorization = CrowdParlay.Social.IntegrationTests.Services.Authorization;
 
 namespace CrowdParlay.Social.IntegrationTests.Tests;
 
@@ -32,10 +34,10 @@ public class AuthorsControllerTests : IClassFixture<WebApplicationContext>
 
         await _harness.Bus.Publish(@event);
 
-        var message = await _client.GetAsync($"api/authors/{@event.UserId}");
+        var message = await _client.GetAsync($"/api/v1/authors/{@event.UserId}");
         message.StatusCode.Should().Be(HttpStatusCode.OK);
 
-        var response = await message.Content.ReadFromJsonAsync<AuthorDto>();
+        var response = await message.Content.ReadFromJsonAsync<AuthorDto>(GlobalSerializerOptions.SnakeCase);
         response.Should().BeEquivalentTo(new AuthorDto
         {
             Id = Guid.Parse(@event.UserId),
@@ -56,21 +58,53 @@ public class AuthorsControllerTests : IClassFixture<WebApplicationContext>
             AvatarUrl: null);
 
         await _harness.Bus.Publish(@event);
-        _client.DefaultRequestHeaders.Add("X-UserId", @event.UserId);
 
-        // Create top-level comment
-        var createCommentResponse = await _client.PostAsJsonAsync("api/comments", new CommentRequest("Top-level comment!"));
-        createCommentResponse.StatusCode.Should().Be(HttpStatusCode.Created, "top-level comment cannot be created");
+        // Create discussion
+        var serializedCreateDiscussionRequest = JsonSerializer.Serialize(
+            new DiscussionRequest("Test discussion", "Something"),
+            GlobalSerializerOptions.SnakeCase);
+
+        var accessToken = Authorization.ProduceJwt(@event.UserId);
+        var createDiscussionResponse = await _client.SendAsync(new HttpRequestMessage(HttpMethod.Post, "/api/v1/discussions")
+        {
+            Content = new StringContent(serializedCreateDiscussionRequest, Encoding.UTF8, "application/json"),
+            Headers = { { "Authorization", $"Bearer {accessToken}" } }
+        });
+
+        createDiscussionResponse.Should().HaveStatusCode(HttpStatusCode.Created);
+        var discussion = await createDiscussionResponse.Content.ReadFromJsonAsync<DiscussionDto>(GlobalSerializerOptions.SnakeCase);
+
+        // Create parent comment
+        var serializedCreateCommentRequest = JsonSerializer.Serialize(
+            new CommentRequest(discussion!.Id, "Top-level comment!"),
+            GlobalSerializerOptions.SnakeCase);
+
+        var createCommentResponse = await _client.SendAsync(new HttpRequestMessage(HttpMethod.Post, "/api/v1/comments")
+        {
+            Content = new StringContent(serializedCreateCommentRequest, Encoding.UTF8, "application/json"),
+            Headers = { { "Authorization", $"Bearer {accessToken}" } }
+        });
+
+        createCommentResponse.StatusCode.Should().Be(HttpStatusCode.Created);
+        var comment = await createCommentResponse.Content.ReadFromJsonAsync<CommentDto>(GlobalSerializerOptions.SnakeCase);
 
         // Create reply comment
-        var comment = await createCommentResponse.Content.ReadFromJsonAsync<CommentDto>();
-        var createReplyResponse = await _client.PostAsJsonAsync($"api/comments/{comment!.Id}/reply", new CommentRequest("Reply comment."));
-        createReplyResponse.StatusCode.Should().Be(HttpStatusCode.Created, "reply comment cannot be created");
+        var serializedCreateReplyRequest = JsonSerializer.Serialize(
+            new ReplyRequest("Reply comment."),
+            GlobalSerializerOptions.SnakeCase);
 
-        // Get top-level comment
-        var getCommentResponse = await _client.GetAsync($"api/comments/{comment.Id}");
-        getCommentResponse.StatusCode.Should().Be(HttpStatusCode.OK, "top-level comment cannot be fetched");
-        comment = await getCommentResponse.Content.ReadFromJsonAsync<CommentDto>();
+        var createReplyResponse = await _client.SendAsync(new HttpRequestMessage(HttpMethod.Post, $"/api/v1/comments/{comment!.Id}")
+        {
+            Content = new StringContent(serializedCreateReplyRequest, Encoding.UTF8, "application/json"),
+            Headers = { { "Authorization", $"Bearer {accessToken}" } }
+        });
+
+        createReplyResponse.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        // Get parent comment
+        var getCommentResponse = await _client.GetAsync($"/api/v1/comments/{comment.Id}");
+        getCommentResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+        comment = await getCommentResponse.Content.ReadFromJsonAsync<CommentDto>(GlobalSerializerOptions.SnakeCase);
 
         comment!.ReplyCount.Should().Be(1);
         comment.FirstRepliesAuthors.Should().ContainSingle(x => x.Id == Guid.Parse(@event.UserId));
