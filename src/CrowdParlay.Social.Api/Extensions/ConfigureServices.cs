8804ba48f88cb7e13d038bef66a38ab63faa772d
Степной ишак <69521267+undrcrxwn@users.noreply.github.com>
2024-04-02 17:50:21 +0300

    feat: remove MassTransit support

--- src/CrowdParlay.Social.Api/Extensions/ConfigureServices.cs
+++ src/CrowdParlay.Social.Api/Extensions/ConfigureServices.cs
@@ -1,7 +1,4 @@
-using CrowdParlay.Communication;
-using CrowdParlay.Social.Api.Consumers;
 using CrowdParlay.Social.Api.Services;
-using MassTransit;
 
 namespace CrowdParlay.Social.Api.Extensions;
 
@@ -9,29 +6,13 @@ public static class ConfigureApiExtensions
 {
     public static IServiceCollection AddApi(this IServiceCollection services, IConfiguration configuration)
     {
-        services
+        services.AddHealthChecks();
+        return services
             .ConfigureEndpoints()
             .ConfigureAuthentication()
             .ConfigureCors(configuration)
             .AddExceptionHandler<GlobalExceptionHandler>()
             .AddProblemDetails()
             .AddAuthorization();
-
-        return services.AddMassTransit(bus =>
-        {
-            bus.AddConsumersFromNamespaceContaining<UserEventConsumer>();
-            bus.UsingRabbitMq((context, configurator) =>
-            {
-                var amqpServerUrl =
-                    configuration["RABBITMQ_AMQP_SERVER_URL"]
-                    ?? throw new InvalidOperationException("Missing required configuration 'RABBITMQ_AMQP_SERVER_URL'.");
-
-                configurator.Host(amqpServerUrl);
-                configurator.ConfigureEndpoints(context);
-                configurator.ConfigureTopology();
-
-                configurator.Message<UserUpdatedEvent>(x => x.SetEntityName("user"));
-            });
-        });
     }
 }
