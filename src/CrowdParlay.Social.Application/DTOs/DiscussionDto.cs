74a5d1785f73f57f711cf49fa604c565638251d2
Степной ишак <69521267+undrcrxwn@users.noreply.github.com>
2024-07-28 20:31:51 +0300

    docs: mark discussion's author as nullable

--- src/CrowdParlay.Social.Application/DTOs/DiscussionDto.cs
+++ src/CrowdParlay.Social.Application/DTOs/DiscussionDto.cs
@@ -5,6 +5,6 @@ public class DiscussionDto
     public required Guid Id { get; set; }
     public required string Title { get; set; }
     public required string Description { get; set; }
-    public required AuthorDto Author { get; set; }
+    public required AuthorDto? Author { get; set; }
     public required DateTimeOffset CreatedAt { get; set; }
 }
