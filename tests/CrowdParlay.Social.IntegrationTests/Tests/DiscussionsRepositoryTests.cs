bba017efdbbccf8187e8d8db4f1bcdfc0c7a3b80
Степной ишак <69521267+undrcrxwn@users.noreply.github.com>
2024-11-09 23:12:48 +0300

    tests(reactions): cover reactions selection and counting when reading discussion

--- tests/CrowdParlay.Social.IntegrationTests/Tests/DiscussionsRepositoryTests.cs
+++ tests/CrowdParlay.Social.IntegrationTests/Tests/DiscussionsRepositoryTests.cs
@@ -45,4 +45,38 @@ public class DiscussionsRepositoryTests(WebApplicationContext context) : IAssemb
         // Assert
         response.Items.Should().BeEmpty();
     }
+
+    [Fact(DisplayName = "Get discussion with reactions")]
+    public async Task GetDiscussionWithReactions()
+    {
+        // Arrange
+        await using var scope = _services.CreateAsyncScope();
+        var authorsRepository = scope.ServiceProvider.GetRequiredService<IAuthorsRepository>();
+        var discussionsRepository = scope.ServiceProvider.GetRequiredService<IDiscussionsRepository>();
+        var reactionsRepository = scope.ServiceProvider.GetRequiredService<IReactionsRepository>();
+
+        var authorId = Guid.NewGuid();
+        await authorsRepository.EnsureCreatedAsync(authorId);
+
+        var viewerId = Guid.NewGuid();
+        await authorsRepository.EnsureCreatedAsync(viewerId);
+
+        await discussionsRepository.CreateAsync(viewerId, "Discussion 1", "bla bla bla");
+        var discussionId = await discussionsRepository.CreateAsync(authorId, "Discussion 2", "bla bla bla");
+
+        await reactionsRepository.SetAsync(discussionId, authorId, new HashSet<string> { "thumb-up", "heart" });
+        await reactionsRepository.SetAsync(discussionId, viewerId, new HashSet<string> { "thumb-up", "rainbow" });
+
+        // Act
+        var discussion = await discussionsRepository.GetByIdAsync(discussionId, viewerId);
+
+        // Assert
+        discussion.ViewerReactions.Should().BeEquivalentTo("thumb-up", "rainbow");
+        discussion.ReactionCounters.Should().BeEquivalentTo(new Dictionary<string, int>
+        {
+            ["thumb-up"] = 2,
+            ["heart"] = 1,
+            ["rainbow"] = 1
+        });
+    }
 }
